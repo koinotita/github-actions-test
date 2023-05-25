21b770156e7f2e7f30a1ba645f418ed8ada738ff
zhuyx <zhuyx@startimes.com.cn>
2023-05-25 16:37:36 +0800

    v2

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World, I'm a Gin server")
+		c.String(200, "Hello World, I'm a Gin server v2 with auto push.")
 	})
 
 	r.Run(":8080")
